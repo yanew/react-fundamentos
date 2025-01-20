7a815db868d940fd9f81ee3d27f512d374d0b471
Yane Wanderley <yanewanderley@gmail.com>
2025-01-20 12:20:37 -0300

    Aula Fundamentos do React - Trabalhando com arquivos css

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,11 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader' ],
+      },
     ],
   },
   resolve: {
