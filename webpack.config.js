aa96b5d903d296ea80842ff57b811cfddaf76610
Yane Wanderley <yanewanderley@gmail.com>
2025-10-21 18:48:51 -0300

    Acrescentando configuração necessária no webpack

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'bundle[hash].js',
+    filename: 'bundle[fullhash].js',
   },
   plugins:[
     new HtmlWebpackPlugin({
